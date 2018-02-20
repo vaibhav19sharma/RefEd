91b60404a6c631725589cba043ed20bea7c8e8c1
vaibhav19sharma <31787798+vaibhav19sharma@users.noreply.github.com>
2018-02-20 01:04:41 -0500

    Adding user information to database

--- app/src/main/java/com/example/vaibh/refed/WelcomeScreenOne.java
+++ app/src/main/java/com/example/vaibh/refed/WelcomeScreenOne.java
@@ -4,23 +4,36 @@ import android.content.Intent;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mukesh.countrypicker.fragments.CountryPicker;
 import com.mukesh.countrypicker.interfaces.CountryPickerListener;
 
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WelcomeScreenOne extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private TextView txtWelcome;
     private EditText edtName,edtAge;
+    private Button btnNext;
     private MultiAutoCompleteTextView mltHost,mltHome;
+    private String name,age,hostName,homeName,uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +47,7 @@ public class WelcomeScreenOne extends AppCompatActivity {
         }
 
         txtWelcome = findViewById(R.id.txtWelcome);
-        txtWelcome.setText("Welcome" + mAuth.getCurrentUser().getEmail().replace("@makefakedomain.com",""));
+        txtWelcome.setText("Welcome " + mAuth.getCurrentUser().getEmail().replace("@makefakedomain.com",""));
 
         edtName = findViewById(R.id.edtName);
         edtAge = findViewById(R.id.edtAge);
@@ -42,8 +55,47 @@ public class WelcomeScreenOne extends AppCompatActivity {
         mltHost = findViewById(R.id.mltHost);
         mltHome = findViewById(R.id.mltHome);
 
-        String name = edtName.getText().toString();
-        String age = edtAge.getText().toString();
+        btnNext = findViewById(R.id.btnSubmit);
+
+        uid = mAuth.getCurrentUser().getUid();
+        DatabaseReference currentUser_read = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+        currentUser_read.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String dataName = dataSnapshot.child("Name").getValue().toString();
+                if(!dataName.isEmpty()){
+                    edtName.setText(dataName);
+                }
+
+                String dataAge = dataSnapshot.child("Age").getValue().toString();
+                Log.i("test",dataAge);
+                if(!dataAge.isEmpty()){
+                    edtAge.setText(dataAge);
+                }
+
+//                String dataHomeCountry = dataSnapshot.child("Home_Country").getValue().toString();
+//                Log.i("test", dataHomeCountry);
+//                if(!dataHomeCountry.isEmpty()){
+//                    mltHome.setText(dataHomeCountry);
+//                }
+//
+//                String dataHostCountry = dataSnapshot.child("Host_Country").getValue().toString();
+//                if(!dataHostCountry.isEmpty()){
+//                    mltHost.setText(dataHostCountry);
+//                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
 
         //Select a Host Country from Drop down
@@ -56,10 +108,10 @@ public class WelcomeScreenOne extends AppCompatActivity {
                     @Override
                     public void onSelectCountry(String name, String code, String dialCode, int flagDrawableResID) {
                         // Implement your code here
-                        String hostName = name;
+                        hostName = name;
                         mltHost.setText(hostName);
                         picker.dismiss();
-                                            }
+                    }
                 });
                 picker.show(getSupportFragmentManager(), "COUNTRY_PICKER");
 
@@ -76,7 +128,7 @@ public class WelcomeScreenOne extends AppCompatActivity {
                     @Override
                     public void onSelectCountry(String name, String code, String dialCode, int flagDrawableResID) {
                         // Implement your code here
-                        String homeName = name;
+                        homeName = name;
                         mltHome.setText(homeName);
                         picker.dismiss();
                     }
@@ -85,6 +137,36 @@ public class WelcomeScreenOne extends AppCompatActivity {
             }
         });
 
+        //OnClick listener on button to store user data onto Firebase
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                name = edtName.getText().toString();
+                age = edtAge.getText().toString();
+                if(name.isEmpty() || age.isEmpty() || mltHost.getText().toString().isEmpty() || mltHome.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter all fields",Toast.LENGTH_LONG).show();
+                }
+                else {
+
+                    DatabaseReference currentUser = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+
+                    Map addInformation = new HashMap();
+                    addInformation.put("Name", name);
+                    addInformation.put("Age", age);
+                    addInformation.put("Host_Country", hostName);
+                    addInformation.put("Home_Country", homeName);
+
+                    currentUser.setValue(addInformation);
+
+                    Intent setAvatar = new Intent(getApplicationContext(), WelcomeScreenTwo.class);
+                    startActivity(setAvatar);
+                }
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/vaibh/refed/WelcomeScreenTwo.java
+++ app/src/main/java/com/example/vaibh/refed/WelcomeScreenTwo.java
@@ -3,12 +3,17 @@ package com.example.vaibh.refed;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.john.waveview.WaveView;
+
 public class WelcomeScreenTwo extends AppCompatActivity {
 
+        private WaveView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen_two);
 
+        wv = findViewById(R.id.wave_view);
+        wv.setProgress(40);
     }
 }
