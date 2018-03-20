39d841856061d8eefc71894b013d2c38b8f97bb4
sarah256 <srieger256@gmail.com>
2018-03-20 19:20:53 -0400

    linked modules page

--- app/src/main/java/com/example/vaibh/refed/ModulesPage.java
+++ app/src/main/java/com/example/vaibh/refed/ModulesPage.java
@@ -32,7 +32,7 @@ public class ModulesPage extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.module_list);
 
         ListAdapter adapter=new ListAdapter(this, itemname, imgid);
         list=(ListView)findViewById(R.id.list);
