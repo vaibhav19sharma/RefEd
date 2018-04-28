33074e32cbde9cd386a81ad59124f5b56a443f96
sarah256 <srieger256@gmail.com>
2018-04-27 22:49:41 -0400

    reading json

--- app/src/main/java/com/example/vaibh/refed/Activity_Math.java
+++ app/src/main/java/com/example/vaibh/refed/Activity_Math.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,9 +26,9 @@ public class Activity_Math extends AppCompatActivity {
 
         c = Activity_Math.this;
 
-        String json = loadJSONFromAsset("assets/fonts/Math_Part_1_Section_1");
+        //String json = loadJSONFromAsset("assets/fonts/Math_Part_1_Section_1");
 
-        /*String json;
+        String json;
         JSONObject obj;
         try {
             InputStream is = getAssets().open("assets/fonts/Math_Part_1_Section_1.json");
@@ -40,13 +42,16 @@ public class Activity_Math extends AppCompatActivity {
             ex.printStackTrace();
         } catch (JSONException ex) {
             ex.printStackTrace();
-        }*/
+        }
+
+        // To pull a value from the JSON file:
+        // String section = obj.getJsonString("Section_Name");
 
 
 
     }
 
-    public String loadJSONFromAsset(String fileName) {
+    /*public String loadJSONFromAsset(String fileName) {
         String json = null;
         org.json.JSONObject obj;
         try {
@@ -63,29 +68,30 @@ public class Activity_Math extends AppCompatActivity {
             ex.printStackTrace();
         }
         return json;
-    }
+    }*/
 
-//    public String loadJSONFromAsset(Context context) {
-//        String json = null;
-//        try {
-//            InputStream is = context.getAssets().open("Math_Part_1_Section_1.json");
-//
-//            int size = is.available();
-//
-//            byte[] buffer = new byte[size];
-//
-//            is.read(buffer);
-//
-//            is.close();
-//
-//            json = new String(buffer, "UTF-8");
-//
-//
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//            return null;
-//        }
-//        return json;
-//
-//    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.ChoiceA:
+                if (checked)
+                    // action
+                    break;
+            case R.id.ChoiceB:
+                if (checked)
+                    // action
+                    break;
+            case R.id.ChoiceC:
+                if (checked)
+                    // action
+                    break;
+            case R.id.ChoiceD:
+                if (checked)
+                    // action
+                    break;
+        }
+    }
 }
